04a30d053586a191faad2e1fd167e227c905dfbd
Anders Furseth <anders.furseth@gmail.com>
2016-02-28 19:10:21 +0100

    Bind *calls* with with-redef
    This allows stubadub to work across threads.

--- src/stubadub/core.clj
+++ src/stubadub/core.clj
@@ -6,13 +6,13 @@
   (let [has-return-value? (= :returns (first opts-and-body))
         body (if has-return-value? (nnext opts-and-body) opts-and-body)
         return-value (when has-return-value? (second opts-and-body))]
-    `(binding [*calls* (if (bound? #'*calls*)
-                         *calls* (atom []))]
-       (with-redefs [~func (fn [& args#]
-                             (swap! *calls*
-                                    conj {:func ~func, :args args#})
-                             ~return-value)]
-         ~@body))))
+    `(with-redefs [*calls* (if (bound? #'*calls*)
+                             *calls* (atom []))
+                   ~func (fn [& args#]
+                           (swap! *calls*
+                                  conj {:func ~func, :args args#})
+                           ~return-value)]
+       ~@body)))
 
 (defn calls-to [func]
   (if (bound? #'*calls*)

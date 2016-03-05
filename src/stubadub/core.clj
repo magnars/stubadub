570cf4c63f59b91d05e72df6e3a3fc2625d0a307
Anders Furseth <anders.furseth@gmail.com>
2016-03-05 18:55:51 +0100

    Support specifying return values by args

--- src/stubadub/core.clj
+++ src/stubadub/core.clj
@@ -4,14 +4,22 @@
 
 (defmacro with-stub [func & opts-and-body]
   (let [has-return-value? (= :returns (first opts-and-body))
-        body (if has-return-value? (nnext opts-and-body) opts-and-body)
-        return-value (when has-return-value? (second opts-and-body))]
+        has-return-value-by-args? (= :returns-by-args (first opts-and-body))
+        has-opts? (or has-return-value? has-return-value-by-args?)
+        body (if has-opts?
+               (nnext opts-and-body)
+               opts-and-body)
+        return-value (when has-opts? (second opts-and-body))
+        args (gensym)]
     `(with-redefs [*calls* (if (bound? #'*calls*)
                              *calls* (atom []))
-                   ~func (fn [& args#]
+                   ~func (fn [& ~args]
                            (swap! *calls*
-                                  conj {:func ~func, :args args#})
-                           ~return-value)]
+                                  conj {:func ~func, :args ~args})
+                           ~(cond
+                              has-return-value? return-value
+                              has-return-value-by-args? `(get ~return-value ~args)
+                              :else nil))]
        ~@body)))
 
 (defn calls-to [func]
